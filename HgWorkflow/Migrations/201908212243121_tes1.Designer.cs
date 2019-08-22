72cdbbff5b45c417f9e320ef69c33e517ac52598
Rahul Singh <mr.rahulsingh@hotmail.com>
2019-08-22 09:45:51 +0200

    Removed migration commits and also to register new user

--- HgWorkflow/Migrations/201908212243121_tes1.Designer.cs
+++ HgWorkflow/Migrations/201908212243121_tes1.Designer.cs
@@ -7,13 +7,13 @@ namespace HgWorkflow.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class newFieldAdded : IMigrationMetadata
+    public sealed partial class tes1 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(newFieldAdded));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(tes1));
         
         string IMigrationMetadata.Id
         {
-            get { return "201908201918150_newFieldAdded"; }
+            get { return "201908212243121_tes1"; }
         }
         
         string IMigrationMetadata.Source
